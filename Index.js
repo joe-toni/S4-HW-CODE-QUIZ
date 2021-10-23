646e22113bafefb04874d67df9c0d834c00f2cab
joe-toni <joefaburrieta@gmail.com>
2021-10-22 21:36:08 -0700

    This version of the assignment has some cleaned up commenting and the added use of local storage as well as a partially working high score screen

--- Index.js
+++ Index.js
@@ -1,7 +1,8 @@
-//We are gonna start off by creating an object list of sample questions for testing that will later be adjusted
-// This list of objects will have a number of attributes for 
-const questions =  
-[
+//Questions Object Array
+    //This object will contain all the questions that our test will use, each object will hold the actual question, the four set slots of answers, and the index indicating the 
+    //correct  answer for future reference.
+    const questions =  
+    [
     {
     question : "This is question #1",
     answerIndex: 0,
@@ -66,23 +67,61 @@ const questions =
     optionC: "C.) answer8c",
     optionD: "D.) answer8d"
     }
-];
+    ];
+//spacing
 
-//Query Selected HTML Elements
-    //Up next we are going to pull in the question container and all its children which we will use to later display one question at a time
+ //Query Selected HTML Elements
+    //This portion contains all the elements we pulled from our Index.html file and set them as global variables for reference through out the script
     const startBtn = document.querySelector(".start_btn");
+
     const  questionContainer = document.querySelector(".question_container");
     const questionDisplay = questionContainer.querySelector(".prompt");
     const answerSlots = questionContainer.querySelectorAll(".answer");
-    const endMessage = document.querySelector(".name_input");
+
+    const gameOver = document.querySelector(".game_over_screen");
+    const gameOverScore = document.querySelector(".your_score");
+    const userName = document.querySelector("input");
+
+    const highScoreBoard = document.querySelector("footer");
+    const highScoreSlots = highScoreBoard.querySelectorAll(".highscore")
 //spacing
 
 //Global Variables
-    //These are the variables that will be used through out our javascript function to keep track of the questions, the score and 
-    //what ever else we might need
+    //These are some of the simpler variables that we will use through out the script to keep track of  whether the test has started, what question we are on for reference, and the players score.
     var started = false;
     var score  = 0;
-    var index = 0;
+    var index = 0;  
+//spacing
+
+//Persisting HighScores
+    //This section establishes the global variable we will use to save all the high scores as an empty array, it then calls on a funtion to determine if local storage already has some scores saved on an object.
+    var highScores = [];
+    storageSettup();
+    function storageSettup()
+    {
+        //This first if statement should assure that our local storage object exists and is meant catch at the start of  a new users playthrough
+        if (localStorage.getItem("highScores") === null)
+        {
+             localStorage.setItem("highScores", JSON.stringify(highScores));        
+        }
+        //This else if  statement should also catch at the begining of a playthrough but will populate our local variable with pre existing high scores
+        else if (!started)
+        {
+            let savedScores = JSON.parse(localStorage.getItem("highScores"));
+            for (let i = 0; i < savedScores.length; i++)
+            {
+                highScores.push(savedScores[i]);
+                console.log(highScores);
+            }
+        }
+        //This is meant to catch at the end of a playthrough and is simply meant to push a new list of scores when the player enters their new informa
+        else
+        {
+            localStorage.setItem("highScores", JSON.stringify(highScores));        
+
+        }
+
+    }
 //spacing
 
 //Random Index Generator
@@ -101,71 +140,114 @@ const questions =
     }
 //spacing
 
+//Setting Quiz Starting Conditions
+    //This function should make sure that all the unused sections are correctly placed at the back, that the local storage is reset to accuratly present values on both 
+    // new starts and retrys, and that the index and score are reset for a new test.
+    function startTheQuiz()
+        {
+            startBtn.style.visibility = "collapse";
+            startBtn.style.zIndex = -10;
 
-//we are going to establish a basic start function that will initialize the quiz with all the correct starting conditions
-function startTheQuiz()
-    {
-        startBtn.style.visibility = "collapse";
-        startBtn.style.zIndex = -10;
+            questionContainer.style.visibility = "visible";
+            questionContainer.style.zIndex = 10;
 
-        questionContainer.style.visibility = "visible";
-        questionContainer.style.zIndex = 10;
+            highScoreBoard.style.visibility = "collapse"
+            highScoreBoard.style.zIndex = -10;
 
-        started = true;
-        score = 0;
-        index = 0;
-        loadTheQuestion();
-    }
+            highScoreBoard.style.visibility = "collapse";
+            highScoreBoard.style.zIndex = -10;
+
+            storageSettup();
+            scramble();
+            started = true;
+            score = 0;
+            index = 0;
+            loadTheQuestion();
+        }
 //spacing
 
-
-//We are going to set up an index that for now will allow us to cylcle through the questions in our questions array with the help of a submit
-//function which will be added to each of our answer options like in the demo provided
-function loadTheQuestion() 
-{
-    for(let i = 0; i < 4; i++)
-    {
-        answerSlots[i].dataset.correct = false;
-    } 
-    answerSlots[questions[questionsIndex[index]].answerIndex].dataset.correct = true;
-
-    questionDisplay.innerHTML = questions[questionsIndex[index]].question;
-    answerSlots[0].innerHTML  = questions[questionsIndex[index]].optionA;
-    answerSlots[1].innerHTML  = questions[questionsIndex[index]].optionB;
-    answerSlots[2].innerHTML  = questions[questionsIndex[index]].optionC;
-    answerSlots[3].innerHTML  = questions[questionsIndex[index]].optionD;
-}
-
-//We have now adjusted this function to catch if the user has given the correct answer as well as correctly tracking the 
-//submissions up intil the last question where a final submission whill trigger an end state for the program.
-function submitAnswer()
-{
-    //This determines which answer was clicked for submission
-    let submission = event.target;
-
-    //This initial started condition currently ensures that questions are not accidentally clickable before the quiz is started
-    if(started)
-    {
-        //This if condition starts by checking if the user is on the final submission and running the function as normal if not
-        if (index === questions.length-1)
+//Reseting the Question Display
+    // This function will reset all the questions answer slots to indicate false or incorrect before populating the html element with the question and answers
+    //indicated by the index that moves through a list of randomized indecies and setting the corresponding slot to correct for score keeping.
+    function loadTheQuestion() 
         {
-            if(submission.dataset.correct === "true")
+            for(let i = 0; i < 4; i++)
             {
-                score++;
-             };
-             questionContainer.style.visibility = "collapse";
-             endMessage.innerHTML = `You are done, your score is ${score}.`;
-        }
-       else 
-        {
-            if(submission.dataset.correct === "true")
-            {
-                score++;
-                console.log(score);
-             };
-             index++;
-             loadTheQuestion()
-        }
-    }
-}
+                answerSlots[i].dataset.correct = false;
+            } 
+            answerSlots[questions[questionsIndex[index]].answerIndex].dataset.correct = true;
 
+            questionDisplay.innerHTML = questions[questionsIndex[index]].question;
+            answerSlots[0].innerHTML  = questions[questionsIndex[index]].optionA;
+            answerSlots[1].innerHTML  = questions[questionsIndex[index]].optionB;
+            answerSlots[2].innerHTML  = questions[questionsIndex[index]].optionC;
+            answerSlots[3].innerHTML  = questions[questionsIndex[index]].optionD;
+        }
+//spacing
+
+//Answer Submission And Quiz Progression
+    //This function will get the element that was clicked and after determining if the test is in progress and the proceed to check if the current submission
+    //is the final question, or not pushing the game to a game over screen if yes.
+    function submitAnswer()
+        {
+            let submission = event.target;
+            //This checks to see if the test is in progress to avoid clicks accidentally triggering a submission
+            if(started)
+            {
+                //This if statement checks to see if the current submission is for the final question in our list so that it may procced  as normal
+                //but end buy triggering the game over screen and hiding the test
+                if (index === questions.length-1)
+                {
+                    if(submission.dataset.correct === "true")
+                    {
+                        score++;
+                     };
+                     questionContainer.style.visibility = "collapse";
+                     gameOver.style.visibility = "visible";
+                     gameOver.style.zIndex = 10;
+                     gameOverScore.innerHTML = `You are done, your score is ${score}.`;
+                }
+                //This simply check to see if the current submission is the correct answer and incramenting the index before reseting the questions
+               else 
+                {
+                    if(submission.dataset.correct === "true")
+                    {
+                        score++;
+                     };
+                     index++;
+                     loadTheQuestion()
+                }
+            }
+        }
+//spacing
+
+//Game Over Screen
+    //This function will display what is essentially the game over prompt which will take the users name and the current score saving it to an object before pushing it to our
+    // local array of user names and scores object.
+     function submitUser()
+     {
+         let newHighScore = 
+         {
+             name: userName.value,
+             hScore: score
+         };
+         highScores.push(newHighScore);
+         storageSettup();
+         gameOver.style.visibility = "collapse";
+         gameOver.style.zIndex = -10;
+         displayHighScores();
+     }
+//spacing
+
+//Display High Scores
+     //This object will take the first ten objects from our local storage created list of scores and display them by filling in their designated slots in the html file.
+     function displayHighScores()
+     {
+         for (let i = 0; i < highScores.length&& i < 10; i++)
+         {
+             highScoreSlots[i].innerHTML = `${highScores[i].name} : ${highScores[i].hScore}`;
+         }
+         highScoreBoard.style.visibility = "visible";
+         highScoreBoard.style.zIndex = 10;
+     }
+//spacing
